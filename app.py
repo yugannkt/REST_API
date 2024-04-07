0a78d7e2c0f33f10fcea3febbf4659a98ea087f7
yugannkt <166296076+yugannkt@users.noreply.github.com>
2024-04-07 16:25:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from flask_mail import Mail, Message
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'planets.db')
-app.config['JWT_SECRET_KEY'] = 'super-secret'  # change this IRL
+app.config['JWT_SECRET_KEY'] = 'super-secret'  
 app.config['MAIL_SERVER'] = 'smtp.mailtrap.io'
-app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME']
-app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD']
+app.config['MAIL_USERNAME'] = os.environ['MAIL_USERNAME'] #set your mail username
+app.config['MAIL_PASSWORD'] = os.environ['MAIL_PASSWORD'] # set your mail password
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
@@ -35,35 +35,17 @@ def db_drop():
 
 @app.cli.command('db_seed')
 def db_seed():
-    mercury = Planet(planet_name='Mercury',
-                     planet_type='Class D',
-                     home_star='Sol',
-                     mass=2.258e23,
-                     radius=1516,
-                     distance=35.98e6)
+    mercury = Planet(planet_name='Mercury', planet_type='Class D', home_star='Sol', mass=2.258e23, radius=1516, distance=35.98e6)
 
-    venus = Planet(planet_name='Venus',
-                         planet_type='Class K',
-                         home_star='Sol',
-                         mass=4.867e24,
-                         radius=3760,
-                         distance=67.24e6)
+    venus = Planet(planet_name='Venus', planet_type='Class K',home_star='Sol', mass=4.867e24, radius=3760, distance=67.24e6)
 
-    earth = Planet(planet_name='Earth',
-                     planet_type='Class M',
-                     home_star='Sol',
-                     mass=5.972e24,
-                     radius=3959,
-                     distance=92.96e6)
+    earth = Planet(planet_name='Earth', planet_type='Class M', home_star='Sol', mass=5.972e24,radius=3959,distance=92.96e6)
 
     db.session.add(mercury)
     db.session.add(venus)
     db.session.add(earth)
 
-    test_user = User(first_name='William',
-                     last_name='Herschel',
-                     email='test@test.com',
-                     password='P@ssw0rd')
+    test_user = User(first_name='William',last_name='Herschel',email='test@test.com', password='P@ssw0rd')
 
     db.session.add(test_user)
     db.session.commit()
